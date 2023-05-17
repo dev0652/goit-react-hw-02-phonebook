868ed9215b4354e96f05f9c60f1bb2aa955de653
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-05-17 06:46:21 +0300

    fixed propTypes

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Label } from './Filter.styled';
 
 const Filter = ({ value, onChange }) => (
@@ -8,3 +9,8 @@ const Filter = ({ value, onChange }) => (
 );
 
 export default Filter;
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
